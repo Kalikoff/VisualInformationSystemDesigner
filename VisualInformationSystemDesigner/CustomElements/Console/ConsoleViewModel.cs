5993ca89bd54145967b32869ce98a8284cd0ee79
Kalmykov Semyon <ksm@shipbuilders.ru>
2024-04-10 16:35:37 +0300

    Добавлен интерпретатор, также его можно запустить из консоли

--- VisualInformationSystemDesigner/CustomElements/Console/ConsoleViewModel.cs
+++ VisualInformationSystemDesigner/CustomElements/Console/ConsoleViewModel.cs
@@ -5,14 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using VisualInformationSystemDesigner.Model;
 using VisualInformationSystemDesigner.Utilities;
 
 namespace VisualInformationSystemDesigner.CustomElements.Console
 {
     public class ConsoleViewModel : BaseViewModel
     {
-        private string _source;
-        public string Source
+        private string? _source;
+		private string? _consoleOutput;
+		private string? _commandText;
+
+		public string? Source
         {
             get { return _source; }
             set
@@ -24,13 +28,67 @@ namespace VisualInformationSystemDesigner.CustomElements.Console
                 }
             }
         }
+		public string? ConsoleOutput
+		{
+			get { return _consoleOutput; }
+			set
+			{
+				if (_consoleOutput != value)
+				{
+					_consoleOutput = value;
+					OnPropertyChanged(nameof(ConsoleOutput));
+				}
+			}
+		}
+		public string? CommandText
+		{
+			get { return _commandText; }
+			set
+			{
+				if (_commandText != value)
+				{
+					_commandText = value;
+					OnPropertyChanged(nameof(CommandText));
+				}
+			}
+		}
 
-        public ConsoleViewModel()
+		public ConsoleViewModel()
         {
-            
+			ProcessCommand = new RelayCommand(ProcessCommandExecute);
         }
 
-        public void UpdateSource(string source)
+		public ICommand ProcessCommand { get; }
+
+		private void ProcessCommandExecute(object obj)
+		{
+			string? command = CommandText?.Trim();
+
+			if (command == null)
+			{
+				return;
+			}
+
+			if (command == "/start")
+			{
+				Interpreter interpreter = new Interpreter();
+				ConsoleOutput += interpreter.Interpret(Source) + "\n";
+			}
+			else if (command == "/clear")
+			{
+				ConsoleOutput = "";
+			}
+			else
+			{
+				ConsoleOutput += "Неизвестная команда.\n";
+			}
+
+			CommandText = "";
+		}
+
+
+
+		public void UpdateSource(string source)
         {
             Source = source;
         }
--- VisualInformationSystemDesigner/Model/Interpreter.cs
+++ VisualInformationSystemDesigner/Model/Interpreter.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Shapes;
 
 namespace VisualInformationSystemDesigner.Model
 {
     public class Interpreter
     {
+		public Interpreter()
+		{
+
+		}
+
         public enum VariableType
         {
             Integer,
             Double,
-            String
+            String,
+			Null
         }
 
         public class Variable
@@ -30,47 +37,73 @@ namespace VisualInformationSystemDesigner.Model
 
         private readonly Dictionary<string, Variable> variables = new Dictionary<string, Variable>();
 
-        public void Interpret(string code)
-        {
-            string[] tokens = code.Split(' ');
+		public string Interpret(string? code)
+		{
+			if (code == null)
+			{
+				return "";
+			}
 
-            for (int i = 0; i < tokens.Length; i++)
-            {
-                if (tokens[i] == "int" || tokens[i] == "double" || tokens[i] == "string")
-                {
-                    VariableType type = VariableType.Integer;
-                    if (tokens[i] == "double")
-                        type = VariableType.Double;
-                    else if (tokens[i] == "string")
-                        type = VariableType.String;
+			string output = "";
 
-                    string name = tokens[i + 1];
-                    object value = null;
+			code = code.Replace("\r", "");
 
-                    if (type != VariableType.String)
-                    {
-                        value = Convert.ChangeType(tokens[i + 3], type == VariableType.Integer ? typeof(int) : typeof(double));
-                    }
-                    else
-                    {
-                        value = tokens[i + 3];
-                    }
+			string[] lines = code.Split('\n');
 
-                    variables[name] = new Variable(type, value);
-                }
-                else if (tokens[i] == "print")
-                {
-                    string name = tokens[i + 1];
-                    if (variables.ContainsKey(name))
-                    {
-                        Console.WriteLine(variables[name].Value);
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Error: Variable '{name}' not found.");
-                    }
-                }
-            }
-        }
+			foreach (string line in lines)
+			{
+				string[] tokens = line.Split(' ');
+
+				for (int i = 0; i < tokens.Length; i++)
+				{
+					if (tokens[i] == "int" || tokens[i] == "double" || tokens[i] == "string")
+					{
+						VariableType type = VariableType.Null;
+
+						if (tokens[i] == "int")
+						{
+							type = VariableType.Integer;
+						}
+						else if (tokens[i] == "double")
+						{
+							type = VariableType.Double;
+						}
+						else if (tokens[i] == "string")
+						{
+							type = VariableType.String;
+						}
+
+						string name = tokens[i + 1];
+						object value;
+
+						if (type != VariableType.String)
+						{
+							value = Convert.ChangeType(tokens[i + 3], type == VariableType.Integer ? typeof(int) : typeof(double));
+						}
+						else
+						{
+							value = tokens[i + 3];
+						}
+
+						variables[name] = new Variable(type, value);
+					}
+					else if (tokens[i] == "print")
+					{
+						string name = tokens[i + 1];
+						if (variables.ContainsKey(name))
+						{
+							//Console.WriteLine(variables[name].Value);
+							output += variables[name].Value + "\n";
+						}
+						else
+						{
+							Console.WriteLine($"Error: Variable '{name}' not found.");
+						}
+					}
+				}
+			}
+
+			return output;
+		}
     }
 }
