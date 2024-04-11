644b19592b590310bca5c3cd39ea59ddc1725717
g403_kalmykov <kalikoff.s@yandex.ru>
2024-04-11 22:47:17 +0300

    Добавления цикла for в интерпретатор

--- VisualInformationSystemDesigner/Model/Interpreter.cs
+++ VisualInformationSystemDesigner/Model/Interpreter.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
+﻿using System.Globalization;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows.Shapes;
-using System.Xml.Linq;
 
 namespace VisualInformationSystemDesigner.Model
 {
@@ -65,60 +58,100 @@ namespace VisualInformationSystemDesigner.Model
 
 			string[] lines = code.Split('\n');
 
-			foreach (string line in lines)
-			{
-				string[] tokens = regex.Matches(line).Cast<Match>().Select(x => x.Value.Trim('"')).ToArray();
+            for (int lineNumber = 0; lineNumber < lines.Length; lineNumber++)
+            {
+                string line = lines[lineNumber];
+                string[] tokens = regex.Matches(line).Cast<Match>().Select(x => x.Value.Trim('"')).ToArray();
 
-				for (int i = 0; i < tokens.Length; i++)
+				if (_validVariableType.Contains(tokens[0]))
 				{
-					if (_validVariableType.Contains(tokens[0]))
+					VariableType type = VariableType.Null;
+					object? value = null;
+
+					switch (tokens[0])
 					{
-						VariableType type = VariableType.Null;
-						object? value = null;
-
-						switch (tokens[i])
-						{
-							case "int":
-								type = VariableType.Integer;
-								value = Convert.ChangeType(tokens[i + 3], typeof(int));
-								break;
-							case "double":
-								type = VariableType.Double;
-								value = double.Parse(tokens[i + 3], CultureInfo.InvariantCulture);
-								break;
-							case "string":
-								type = VariableType.String;
-								value = tokens[i + 3];
-								break;
-						}
-
-						string name = tokens[i + 1];
-						variables[name] = new Variable(type, value);
-
-						break;
+						case "int":
+							type = VariableType.Integer;
+							value = Convert.ChangeType(tokens[3], typeof(int));
+							break;
+						case "double":
+							type = VariableType.Double;
+							value = double.Parse(tokens[3], CultureInfo.InvariantCulture);
+							break;
+						case "string":
+							type = VariableType.String;
+							value = tokens[3];
+							break;
 					}
-					else if (tokens[0] == "print")
+
+					string name = tokens[1];
+					variables[name] = new Variable(type, value);
+
+					break;
+				}
+				else if (tokens[0] == "print")
+				{
+					string name = tokens[1];
+
+					if (variables.ContainsKey(name))
 					{
-						string name = tokens[i + 1];
-
-						if (variables.ContainsKey(name))
-						{
-							//Console.WriteLine(variables[name].Value);
-							output += variables[name].Value + "\n";
-						}
-						else
-						{
-							output += $"Ошибка: Переменная '{name}' не найдена." + "\n";
-							//Console.WriteLine($"Error: Variable '{name}' not found.");
-						}
-
-						break;
+						//Console.WriteLine(variables[name].Value);
+						output += variables[name].Value + "\n";
 					}
 					else
 					{
-						return $"Ошибка: Такой команды не существует '{tokens[i]}'!" + "\n";
+						output += $"Ошибка: Переменная '{name}' не найдена." + "\n";
+						//Console.WriteLine($"Error: Variable '{name}' not found.");
+					}
+
+					break;
+				}
+				else if (tokens[0] == "for")
+				{
+                    if (tokens.Length != 4 || tokens[2] != "in")
+                    {
+                        return "Ошибка в синтаксисе цикла 'for'!" + "\n";
+                    }
+
+                    string counterName = tokens[1];
+                    int iterations;
+
+                    if (int.TryParse(tokens[3], out iterations))
+                    {
+                        for (int i = 0; i < iterations; i++)
+                        {
+                            variables[counterName] = new Variable(VariableType.Integer, i);
+                            string loopBody = "";
+                            int braceCount = 0;
+                            while (lineNumber < lines.Length)
+                            {
+                                line = lines[lineNumber];
+                                int startIndex = line.IndexOf('{');
+                                if (startIndex != -1)
+                                {
+                                    braceCount++;
+                                }
+                                int endIndex = line.IndexOf('}');
+                                if (endIndex != -1)
+                                {
+                                    braceCount--;
+                                }
+                                loopBody += line;
+                                if (braceCount == 0)
+                                {
+                                    break;
+                                }
+                                lineNumber++;
+                            }
+                            output += Interpret(loopBody);
+                        }
+                    }
+					else
+					{
+						return $"Ошибка: Такой команды не существует '{tokens}'!" + "\n";
 					}
 				}
+
 			}
 
 			return output;
