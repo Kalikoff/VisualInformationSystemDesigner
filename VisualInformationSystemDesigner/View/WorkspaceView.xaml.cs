69b8409e988cd027f1dd5471eafc678ca6f9902f
g403_kalmykov <kalikoff.s@yandex.ru>
2024-04-17 18:22:30 +0300

    Обноление названия страницы Workspace

---
+++ VisualInformationSystemDesigner/View/WorkspaceView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace VisualInformationSystemDesigner.View
+{
+    public partial class WorkspaceView : Page
+    {
+        public WorkspaceView()
+        {
+            InitializeComponent();
+        }
+    }
+}
